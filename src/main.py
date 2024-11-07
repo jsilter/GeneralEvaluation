e3ab0b2aa7dde0c13865372a117c8dd1834df6d3
Jacob Silterra <silterra@mit.edu>
2024-11-07 11:56:38 -0500

    Add basic stats table at the beginning

--- src/main.py
+++ src/main.py
@@ -5,10 +5,13 @@ Run set of evaluations on collected data validation.
 We expect a datasheet of patient and exam information.
 """
 
-import datetime
 import argparse
+import datetime
+import math
 import os
+import pprint
 
+import numpy as np
 import pandas as pd
 import matplotlib
 from matplotlib import pyplot as plt
@@ -73,51 +76,115 @@ def generate_true_columns(input_df, exam_date_col, diagnosis_date_col, true_pref
 
     return input_df
 
+def create_basic_stats_table(input_df):
+    basic_stats = {"Total": len(input_df),
+                   "Positive": int(input_df.apply(_is_positive, axis=1).sum())}
+    month_intervals = [(-np.inf, 0), (0, 3), (3, 12), (12, 24), (24, 36), (36, 48), (48, 60), (60, np.inf)]
+    for start, end in month_intervals:
+        if start is None or start == -np.inf:
+            start_str = "-Inf"
+        else:
+            start_str = f"{start} months"
+        if end is None or end == np.inf:
+            end_str = "Inf"
+        else:
+            end_str = f"{end} months"
+
+        key = f"{start_str} to {end_str}"
+
+        keep_rows = input_df["__interval_months"].between(start, end, inclusive="right")
+        basic_stats[key] = int(keep_rows.sum())
+
+    # pprint.pprint(basic_stats)
+    basic_stats_df = pd.DataFrame([basic_stats]).T
+    basic_stats_df.columns = ["Count"]
+    fraction = basic_stats_df["Count"] / basic_stats_df.loc["Total", "Count"]
+    # basic_stats_df["Percent"] = (100.0*fraction).round(2)
+    basic_stats_df["Count"] = basic_stats_df["Count"].astype(int)
+    basic_stats_df = basic_stats_df.reset_index()
+    # Create a figure and axis
+    fig, ax = plt.subplots(figsize=(8, 2))  # Adjust the size as needed
+    # Create the table
+    table = plt.table(cellText=basic_stats_df.values,
+                      colLabels=basic_stats_df.columns,
+                      cellLoc='center', loc='center')
+    df = basic_stats_df
+    col_width = 1.0 / df.shape[1]
+    table.auto_set_font_size(False)
+    for key, cell in table.get_celld().items():
+        if key[0] == 0:
+            cell.set_text_props(fontweight='bold')
+        cell.set_width(col_width)
+
+    ax.add_table(table)
+    ax.axis('tight')
+    ax.axis('off')
+
+    return fig, basic_stats_df
+
+
 
 
 if True and __name__ == "__main__":
     # input_path = "/Users/silterra/chem_home/Sybil/nlst_predictions/sybil_ensemble_calibrated_v2.csv"
-    # input_path = "/Users/silterra/Projects/Mirai_general/Apollo/3081_Validation_score.xlsx"
-    input_path = "/Users/silterra/Projects/Mirai_general/2024_ShefaOrman/Copy of final 602 patient Mirai sheet.xlsx"
+    input_path = "/Users/silterra/Projects/Mirai_general/Apollo/3081_Validation_score.xlsx"
+    diagnosis_date_col = "Biopsy_positive_date"
+    exam_date_col = "Mammogram_done_date"
+    skiprows = 0
+    # input_path = "/Users/silterra/Projects/Mirai_general/2024_ShefaOrman/Copy of final 602 patient Mirai sheet.xlsx"
+    # exam_date_col = "exam date"
+    # diagnosis_date_col = "Cancer Diagnosis Date (yyyy-mm)"
+    # skiprows = 1
     split_col = "Year"
-    exam_date_col = "exam date"
-    diagnosis_date_col = "Cancer Diagnosis Date (yyyy-mm)"
     cat_name = "Year"
+    # Require diagnosis be at least 3 months after exam
+    # min_months = 3
+    min_months = 3
     categories = [
-        {"name": "Year 1", "pred_col": "Year 1", "true_col": "true_year1"},
-        {"name": "Year 2", "pred_col": "Year 2", "true_col": "true_year2"},
-        {"name": "Year 3", "pred_col": "Year 3", "true_col": "true_year3"},
-        {"name": "Year 4", "pred_col": "Year 4", "true_col": "true_year4"},
-        {"name": "Year 5", "pred_col": "Year 5", "true_col": "true_year5"},
+        {"name": "Year 1", "pred_col": "year_1", "true_col": "true_year1"},
+        {"name": "Year 2", "pred_col": "year_2", "true_col": "true_year2"},
+        {"name": "Year 3", "pred_col": "year_3", "true_col": "true_year3"},
+        {"name": "Year 4", "pred_col": "year_4", "true_col": "true_year4"},
+        {"name": "Year 5", "pred_col": "year_5", "true_col": "true_year5"},
     ]
 
     output_path = os.path.join(os.path.dirname(input_path), "evaluation_report.pdf")
 
     input_name = os.path.basename(input_path)
-    input_df = gel.load_input_df(input_name, input_path, skiprows=1)
+    input_df = gel.load_input_df(input_name, input_path, skiprows=skiprows)
 
     print(f"Loaded {len(input_df)} rows from {input_path}")
+    # print(f"Columns: {input_df.columns}")
+
+    def _is_positive(row):
+        return str(row[diagnosis_date_col]).lower() not in {"-1", "nan", "nat", ""}
+
+    def _is_negative(row):
+        return not _is_positive(row)
 
     # Exclude rows where the diagnosis date is before the exam date
-    def _keep_row(row):
+    def _keep_row(row, min_months=min_months):
         # Keep the negatives
-        if str(row[diagnosis_date_col]) == "-1":
+        if _is_negative(row):
             return True
 
         # Exclude cases where the exam is after the diagnosis
         date_diff = _subtract_dates(row[diagnosis_date_col], row[exam_date_col])
-        # Require diagnosis be at least 3 months after exam
-        min_months = 3
         return date_diff >= min_months
-
-    keep_rows = input_df.apply(_keep_row, axis=1)
-    print(f"Keeping {keep_rows.sum()} / {len(keep_rows)} rows with valid exam/diagnosis dates")
-    input_df = input_df.loc[keep_rows, :]
 
     # Read dates for mammogram and biopsy, turn into binary columns for each year
     generate_true_columns(input_df, exam_date_col, diagnosis_date_col, num_years=5)
 
+    fig, basic_stats_df = create_basic_stats_table(input_df)
+
     pdf_pages = PdfPages(output_path)
+    pdf_pages.savefig(fig)
+    plt.close(fig)
+
+    if min_months is not None:
+        keep_rows = input_df.apply(_keep_row, axis=1)
+        print(f"Keeping {keep_rows.sum()} / {len(keep_rows)} rows with valid exam/diagnosis dates")
+        input_df = input_df.loc[keep_rows, :]
 
     curves_by_cat = {}
     stats_by_cat = {}
@@ -140,7 +207,7 @@ if True and __name__ == "__main__":
     # ROC Curves
     plot_roc_curves = True
     if plot_roc_curves:
-        fig, ax = plot_roc_prc(curves_by_cat, stats_by_cat, "NLST")
+        fig, ax = plot_roc_prc(curves_by_cat, stats_by_cat, "Apollo Validation")
         pdf_pages.savefig(fig)
         plt.close(fig)
 
