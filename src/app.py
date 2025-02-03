c33da73f1083f92fbf1d488d6f5c3cec917129f5
Jacob Silterra <silterra@mit.edu>
2025-02-03 12:09:59 -0500

    Add instructions about input datasheet and example

--- src/app.py
+++ src/app.py
@@ -32,9 +32,31 @@ if __name__ == "__main__":
     ds_name = st.text_input("Dataset Name", value="My Dataset")
 
     uploaded_file = st.file_uploader("Results table", type=["csv", "tsv", "xls", "xlsx"])
+    st.markdown("""Upload a file containing the results of the model. The file should contain the following columns:   
+                - `candx_days`: Days between exam and cancer diagnosis. '-1' indicates no cancer diagnosis.  
+                - `fup_days`: Days between exam and last follow-up.   
+                - `Year1`: Model prediction for year 1.  
+                - `Year2`: Model prediction for year 2.  
+                - `Year3`: Model prediction for year 3.  
+                - `Year4`: Model prediction for year 4.  
+                - `Year5`: Model prediction for year 5.  
+                - `Year6`: Model prediction for year 6. (optional)
+    """)
 
-    ppv_target = st.number_input("PPV Target", value=0.85, help="The target PPV value for the model. "
-                                                                "Used for calculating a threshold to achieve the target PPV.")
+    # Example data file
+    example_data_file_path = "data/sybil_ensemble_for_eval_test.csv"
+    example_data_file_name = example_data_file_path.split("/")[-1]
+    with open(example_data_file_path, "rb") as file:
+        data_bytes = file.read()
+
+    st.download_button(
+        label="Download Example Data File",
+        data=data_bytes,
+        file_name=example_data_file_name,
+        mime="text/plain"
+    )
+
+    recall_target = st.number_input("Recall Target", value=0.85, help="The target recall value for the model. ")
 
     run_button = st.button("Run Evaluation", disabled=uploaded_file is None)
 
@@ -46,7 +68,7 @@ if __name__ == "__main__":
             temp_file.write(uploaded_file.getvalue())
             temp_file_path = temp_file.name
 
-        output_file = run_full_eval(ds_name, temp_file_path, ppv_target=ppv_target)
+        output_file = run_full_eval(ds_name, temp_file_path, recall_target=recall_target)
         st.write("Evaluation complete!")
         output_file_name = output_file.split("/")[-1]
 
